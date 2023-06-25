8e46910d3b1bcd0e2f881d1c905244fdaeda02ab
datdx <dathalongibay@gmail.com>
2023-06-25 19:30:33 +0700

    update Demo.js and info.js

--- code/d15/react-start1/src/Demo.js
+++ code/d15/react-start1/src/Demo.js
@@ -1,4 +1,12 @@
-import { name,age,address,gender } from "./info"
+
+/* 
+TH1 : khi import 1 biến/hàm từ file có cú pháp export default thì 
+khi import vào không cần để trong {} : import A from 'path'
+trong đó A là tên biến hay tên hàm 
+
+TH2 : khi import những biến/ham từ file có cu pháp export {var1,var2...} thì khi import {var1,var2} from 'path'
+ */
+import tinhDiemTB,{ name,age,address,gender,xepLoai } from "./info"
 
 function Demo() {
     // React JSX
@@ -11,6 +19,8 @@ function Demo() {
                 <li>Tuổi : {age}</li>
                 <li>Địa chỉ : {address}</li>
                 <li>Giới tính : {gender.toString()}</li>
+                <li>Điểm TB : { tinhDiemTB() }</li>
+                <li>Xếp loại : { xepLoai() }</li>
             </ul>
         </div>
     );
--- code/d15/react-start1/src/info.js
+++ code/d15/react-start1/src/info.js
@@ -3,7 +3,26 @@ let age=21
 let address="hà nội"
 let gender=true
 
+
+function tinhDiemTB() {
+    let toan = 9
+    let ly = 8
+    let hoa = 6
+    let tb = (toan+ly+hoa)/3;
+    return tb;
+}
+
+function xepLoai() {
+    return "Khá";
+}
+
+
 // export ( xuất bình thường )
 // export default ( xuất mặc định )
 
-export {name, age, address, gender}
+// xuất thông thường : export {var1, var2, var3 ...}
+export {name, age, address, gender, xepLoai}
+
+// chỉ xuất đc 1 biến hoặc 1 hàm mặc định : 
+//export default TenBien||TenHam
+export default tinhDiemTB;
