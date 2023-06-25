3af08f9619d00b9c1d713548863498d595813276
datdx <dathalongibay@gmail.com>
2023-06-25 19:15:43 +0700

    update 1 file and create 1 file

--- code/d15/react-start1/src/Demo.js
+++ code/d15/react-start1/src/Demo.js
@@ -1,10 +1,17 @@
-
+import { name,age,address,gender } from "./info"
 
 function Demo() {
+    // React JSX
     return (
         <div className="container demo">
             <p>Thẻ p component demo</p>
             <a href="#">Thẻ a trong component demo</a>
+            <ul>
+                <li>Tên : {name}</li>
+                <li>Tuổi : {age}</li>
+                <li>Địa chỉ : {address}</li>
+                <li>Giới tính : {gender.toString()}</li>
+            </ul>
         </div>
     );
 }
---
+++ code/d15/react-start1/src/info.js
@@ -0,0 +1,9 @@
+let name="Nguyễn văn bình"
+let age=21
+let address="hà nội"
+let gender=true
+
+// export ( xuất bình thường )
+// export default ( xuất mặc định )
+
+export {name, age, address, gender}
