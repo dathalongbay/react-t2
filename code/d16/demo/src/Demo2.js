ce3dffa2a72e2b7f675fe480e5451dcf5e3a086a
datdx <dathalongibay@gmail.com>
2023-06-28 19:10:46 +0700

    create 1 file and update 1 file

---
+++ code/d16/demo/src/Demo2.js
@@ -0,0 +1,14 @@
+
+
+function Demo2() {
+    return (
+        <>
+            <div className="from-login">
+                <label>Email :</label>
+                <input type="text" name="user_email" value="">
+            </div>
+        </>
+    );
+}
+
+export default Demo2;
--- code/d16/demo/src/index.js
+++ code/d16/demo/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Demo from './Demo';
+import Demo2 from './Demo2'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Demo />
+    <Demo2 />
   </React.StrictMode>
 );
 
