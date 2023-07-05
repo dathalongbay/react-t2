39fe068b8442a7a5b2c1a9f52914e97b062863c6
datdx <dathalongibay@gmail.com>
2023-07-05 21:47:49 +0700

    update 2 files and create 7 files

---
+++ code/d18/demo/src/Demo.js
@@ -0,0 +1,16 @@
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
+
+
+function Demo() {
+
+
+    return (<>
+        DEMO
+        </>)
+}
+
+export default Demo;
