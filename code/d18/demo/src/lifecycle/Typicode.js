421a615dd001d36f50a31e824972a25206b45948
datdx <dathalongibay@gmail.com>
2023-07-09 20:46:53 +0700

    update Typicode.js

--- code/d18/demo/src/lifecycle/Typicode.js
+++ code/d18/demo/src/lifecycle/Typicode.js
@@ -11,6 +11,10 @@ class Typicode extends React.Component {
         }
     }
 
+    handleClick = () => {
+        alert('handle click');
+    }
+
     // After rendering (DOM available)
     componentDidMount() {
         // viết js thuần trong đây 
@@ -33,11 +37,26 @@ class Typicode extends React.Component {
                 this.setState({posts: json})
             })
 
+        document.getElementById('box')
+            .addEventListener('click', this.handleClick)
+
+        /* 
+        nếu dùng addEventListener hoặc setTimeout hoặc setInterval trong componentDidMount mà ko clear trong componentWillUnmount thì có thể xảy ra 
+        memory leak ( rò rỉ bộ nhớ ) với vài nghìn component 
+         */    
+
     }
 
     componentWillUnmount() {
-        // khi nào Unmout 
-        
+        // khi nào Unmount ? khi chúng ta clear 1 số tác vụ gây rò rỉ bộ nhớ trong componentDidMount
+        document.getElementById('box')
+        .removeEventListener('click', this.handleClick)
+
+        /*
+        addEventListener >< removeEventListener
+        setTimeout >< clearTimeout
+        setInterval >< clearInterval
+        */
     }
 
     render() {
@@ -45,7 +64,7 @@ class Typicode extends React.Component {
         // JSX 
         return (
             <>
-                <h1>Demo component lifecycle</h1>
+                <h1 id='box'>Demo component lifecycle</h1>
 
                 { this.state.posts.length > 0 && this.state.posts.map(post => (<div className="demo" key={post.id}>
                     <p>{post.id} - {post.title}</p>
