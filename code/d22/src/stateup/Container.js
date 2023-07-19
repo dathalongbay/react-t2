84503d47b690bb95a044096d513bd161e936c07a
datdx <dathalongibay@gmail.com>
2023-07-19 20:17:52 +0700

    update Container.js and TodosApp.js

--- code/d22/src/stateup/Container.js
+++ code/d22/src/stateup/Container.js
@@ -11,10 +11,20 @@ const Container = () => {
         setCount( prevCount => prevCount+1)
     }
 
+    const handleAddTodo = (newTodo) => {
+        let newTodosArray = [...todos, newTodo];
+        setTodos(newTodosArray);
+    }
+
     return (
         <>
             <div>
-                <TodosApp value={todos} demo={1} />
+                <h1>Demo state up</h1>
+                <pre>
+                    Ngoài việc truyền props từ component cha xuống component như dạng 1 biết state như todos bên dưới 
+                    Thì chúng ta có thể truyền prop dưới dạng 1 hàm function
+                </pre>
+                <TodosApp value={todos} demo={1} handleAddTodo={handleAddTodo} />
                 <hr />
 
                 <p>Set increment count</p>
--- code/d22/src/stateup/TodosApp.js
+++ code/d22/src/stateup/TodosApp.js
@@ -1,4 +1,4 @@
-import {memo} from 'react'
+import {memo, useState} from 'react'
 /* Using memo will cause React to skip rendering a component if its props have not changed.
 
 This can improve performance.
@@ -8,13 +8,26 @@ props của nó không thay đổi
 memo có thể cải thiện tốc độ/hiệu suất . 
 */
 
-const TodosApp = ({value, demo}) => {
+const TodosApp = (props) => {
+
+    const [newTodo, setNewTodo] = useState('')
 
     console.log("todo rendering");
-    console.log(value);
+    console.log(props.value);
+
+    const handeAdd = () => {
+        // gửi dữ liệu lên component cha
+        props.handleAddTodo(newTodo);
+        setNewTodo('');
+    }
 
     return (<>
-            {value.length > 0 && value.map((todoItem, index) => {
+
+            <div>
+                <input name='newTodo' value={newTodo} type='text' onChange={(evt) => setNewTodo(evt.target.value)} />
+                <button onClick={handeAdd}>Add todo</button>
+            </div>
+            {props.value.length > 0 && props.value.map((todoItem, index) => {
                 return <div key={index}>{todoItem}</div>
             })}
         </>);
