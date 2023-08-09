a241f05eb45f7c4c21e5c0931673800df01f04c7
datdx <dathalongibay@gmail.com>
2023-08-09 19:11:45 +0700

    update ProductItem.js

--- meshop - Copy/src/components/ProductItem.js
+++ meshop - Copy/src/components/ProductItem.js
@@ -16,7 +16,7 @@ const ProductItem = ({key, product}) => {
             <div key={key} className="bg-white rounded-lg shadow-md p-4 flex flex-col justify-between">
                 <img src={product?.image}
                     alt={product?.title} className=" mb-4 rounded-lg" />
-                <h2 className="text-lg font-semibold" onClick={() => navigate(`product/${product?.id}`)}>{product?.title}</h2>
+                <h2 className="text-lg font-semibold" onClick={() => navigate(`/product/${product?.id}`)}>{product?.title}</h2>
                 <p className="text-gray-600">Giá: ${product?.price}</p>
                 <button className="bg-purple-600 text-white font-semibold px-4 py-2 mt-4 rounded-lg hover:bg-blue-600" onClick={() => handleAddToCart(product)}>Add
                     to Cart</button>
